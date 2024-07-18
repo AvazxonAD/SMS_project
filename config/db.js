a7f8ca29b700ea9f1c2eb7da15038bdff5273af8
AvazxonAD <asqarovavazxon77@gmail.com>
2024-07-18 13:46:38 +0500

    sms

--- config/db.js
+++ config/db.js
@@ -1,11 +1,7 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-  user: 'postgres',
-  password: '1101jamshid',
-  database: 'sms',
-  host: 'localhost',
-  port: 5432
+  connectionString: process.env.POSTGRES_URL,
 })
 
 module.exports = pool
