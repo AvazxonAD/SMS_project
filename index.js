f7b6a9bfaf9575253fbfb86522ffdfc6c52e6345
AvazxonAD <asqarovavazxon77@gmail.com>
2024-07-18 14:16:13 +0500

    sms

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
 const express = require('express')
+const app = express()
+
 require('dotenv').config()
 const exphbs = require('express-handlebars')
 const path = require('path')
 const flash = require('connect-flash')
-const session = require('express-session') 
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const { Pool } = require('pg');
 
-const app = express()
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use(session({
+  store: new pgSession({
+    pool: pool,
+    tableName: 'session'
+  }),
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 kunlik cookie muddati
+}));
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// Sessiyalarni sozlash
-app.use(session({
-    secret: process.env.SECRET_KEY, 
-    resave: false,
-    saveUninitialized: false
-}))
 
 app.use(flash())
 
