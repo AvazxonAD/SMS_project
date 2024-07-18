239f5a2f8a43f688ce3612a8f2097dfd8e4e65eb
AvazxonAD <asqarovavazxon77@gmail.com>
2024-07-18 17:14:27 +0500

    sms

--- config/db.js
+++ config/db.js
@@ -1,10 +1,9 @@
-const Pool = require('pg').Pool
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
   ssl: {
     rejectUnauthorized: false
   }
-})
-
-module.exports = pool
+});
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const express = require('express');
-const app = express();
-const exphbs = require('express-handlebars');
-const path = require('path');
-const flash = require('connect-flash');
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+const exphbs = require('express-handlebars')
+const path = require('path')
+const flash = require('connect-flash')
 const session = require('express-session');
-const pool = require('./config/db');
 const pgSession = require('connect-pg-simple')(session);
+const { Pool } = require('pg');
 
-// Load environment variables
-require('dotenv').config();
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-// Session configuration
 app.use(session({
   store: new pgSession({
     pool: pool,
@@ -19,28 +24,28 @@ app.use(session({
   secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days cookie expiration
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 kunlik cookie muddati
 }));
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
 
-// Handlebars setup
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+app.use(flash())
+
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views')); // Ensure correct path resolution
 
-// Custom middleware or initialization
-require('./utils/create.user')();
+require('./utils/create.user')()
 
-// Routes
-app.use('/auth', require('./routes/auth.route'));
-app.use('/sms', require('./routes/sms.route'));
+app.use('/auth', require('./routes/auth.route'))
+app.use('/sms', require('./routes/sms.route'))
 
-// Server start
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000
+
 app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+    console.log(`Server running on port: ${PORT}`)
+})
