9c8b0634b4444a8062e79d01906bfebeaf06b66e
AvazxonAD <asqarovavazxon77@gmail.com>
2024-07-18 16:03:04 +0500

    sms

--- config/database.sql
+++ config/database.sql
@@ -2,3 +2,9 @@ CREATE TABLE user(
     username VARCHAR(100) NOT NULL,
     password VARCHAR(100)  NOT NULL
 )
+
+CREATE TABLE sessions (
+    sid VARCHAR NOT NULL PRIMARY KEY,
+    sess JSON NOT NULL,
+    expired TIMESTAMP(6) NOT NULL
+);
