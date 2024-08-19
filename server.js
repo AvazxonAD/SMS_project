d6b3f2c8b7f70b374aa47b70f6f178a7de509489
AvazxonAD <asqarovavazxon77@gmail.com>
2024-08-19 19:55:08 +0500

    sms

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/xorazm', require('./routes/sms.xorazm.router.js'))
 
 app.use(require('./middlewares/errorHandler'))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
