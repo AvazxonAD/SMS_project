1f01cb6791efab516736bc7000f1114aed18efe2
AvazxonAD <asqarovavazxon77@gmail.com>
2024-08-23 18:25:00 +0500

    milliy

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ require('dotenv').config();
 require('colors')
 require('./utils/create.user')();
 
-app.use(cors({
-    origin: 'https://sms-project-nine.vercel.app'
-}));
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
