9d15bbb99bd8428e4e6a27013d987b645b19c473
AvazxonAD <asqarovavazxon77@gmail.com>
2024-08-23 21:11:34 +0500

    milliy

--- server.js
+++ server.js
@@ -8,23 +8,14 @@ require('colors')
 require('./utils/create.user')();
 
 const corsOptions = {
-    origin: '*',
+    origin: 'https://sms-project-nine.vercel.app', // Bu yerni aniq domen bilan almashtiring
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   };
   
-  app.options('*', cors(corsOptions));
+  app.use(cors(corsOptions));
   
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*"); // Specific origin kiritish mumkin
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-    }
-    next();
-  });
   
 
 app.use(express.json())
