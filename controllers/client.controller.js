841bbf74468d2a84390bb949ec8a40655a879971
AvazxonAD <asqarovavazxon77@gmail.com>
2024-08-27 17:37:53 +0500

    sms

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -190,7 +190,7 @@ exports.searchOneRegion = asyncHandler(async (req, res, next) => {
 
 // for checked 
 exports.forchecked = asyncHandler(async (req, res, next) => {
-    const clients = await pool.query(`SELECT clients.id, clients.username, clients.phone, clients.region_id, regions.name 
+    const clients = await pool.query(`SELECT clients.id, clients.username, clients.phone, clients.region_id, regions.name AS region_name 
         FROM clients
         JOIN regions ON regions.id = clients.region_id
         WHERE clients.user_id = $1
